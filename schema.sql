2447aa0ced5588ec550c84143b190927d8ae672f
Brian Chen <brianchen0810@hotmail.com>
2020-08-25 00:29:28 -0400

    Final database and schema

--- schema.sql
+++ schema.sql
@@ -3,14 +3,14 @@ DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE professor (
     id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT UNIQUE NOT NULL,
-    overall_rating INTEGER NOT NULL,
-    classes TEXT NOT NULL,
-    reviews TEXT NOT NULL
+    name TEXT NOT NULL,
+    overall_rating TEXT NOT NULL,
+    classes TEXT NOT NULL
 );
 
 CREATE TABLE reviews (
-    id INTEGER PRIMARY KEY NOT NULL,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    rate_my_professor_id INTEGER NOT NULL,
     professor_id INTEGER NOT NULL,
     comment TEXT NOT NULL,
     FOREIGN KEY (professor_id) REFERENCES professor(id)
