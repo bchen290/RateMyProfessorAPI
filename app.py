abe6ea5caeaaeaa3114dfa118be745a34f6a39df
Brian Chen <brianchen0810@hotmail.com>
2020-08-26 00:43:12 -0400

    Add endpoints to retrieve professors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from . import db, RateMyProfessorScraper
 
 DREXEL_ID = 1521
@@ -7,9 +7,38 @@ app = Flask(__name__)
 db.init_app(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Rate My Professor API</h1>
+    <p>A WIP api that fetches data about Drexel's professors</p>'''
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/professors/all', methods=['GET'])
+def professors():
+    cursor = db.get_db().cursor()
+    results = cursor.execute('SELECT * FROM professor').fetchall()
+    items = [dict(zip([key[0] for key in cursor.description], row)) for row in results]
+    return jsonify(items)
+
+
+@app.route('/professors', methods=['GET'])
+def professor():
+    query_parameters = request.args
+
+    professor_name = query_parameters.get('name')
+
+    query = "SELECT * FROM professor WHERE name=?"
+    cursor = db.get_db().cursor()
+    results = cursor.execute(query, (professor_name,)).fetchall()
+
+    items = [dict(zip([key[0] for key in cursor.description], row)) for row in results]
+
+    return jsonify(items)
 
 
 @app.route('/scrape')
