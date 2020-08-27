43bfddbe180a2c156fdf0e8793c5066d3a8bfa01
Brian Chen <brianchen0810@hotmail.com>
2020-08-27 16:51:06 -0400

    Remove pretty printing from json

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
