7faf6f113e2e83d063a888a877fd2cb7a71dc171
Brian Chen <brianchen0810@hotmail.com>
2020-08-27 00:36:30 -0400

    Disable SQLALCHEMY track modification

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app import routes
